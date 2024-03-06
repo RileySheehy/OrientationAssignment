ccd5996d8f14d968c82e5d3e24c5d24929718407
RileySheehy <rileysheehy@yahoo.com>
2024-03-06 16:14:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/gyroscopeassignment/MainActivity.java
@@ -0,0 +1,16 @@
+// Group members: Riley Sheehy & Landon Jones
+
+package com.example.gyroscopeassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
